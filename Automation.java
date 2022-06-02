d0d513b22b704160354d458fb0235461cf9ad6e9
Manikanta Katkuri <ManikantaKatkuri@users.noreply.github.com>
2022-06-02 21:10:32 +0530

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,28 @@
+package week3.day2;
+
+public class Automation extends MultipleLangauge {
+
+	public void selenium() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void java() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ruby() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		Automation au =new Automation();
+		au.java();
+		au.ruby();
+		au.selenium();
+		au.python();
+	}
+}
